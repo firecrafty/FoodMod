4c9b5db34a97106ce208f260f084ea710722ee56
firecrafty <bluer2288@students.pike.k12.in.us>
2016-03-23 21:03:45 -0400

    Decide on a naming scheme

--- src/main/java/com/github/firecrafty/minecraft/foodmod/FoodMod.java
+++ src/main/java/com/github/firecrafty/minecraft/foodmod/FoodMod.java
@@ -34,7 +34,7 @@ import net.minecraft.creativetab.CreativeTabs;
 public class FoodMod {
     //Overrides mcmod.info
     public static final String MODID = "foodmod";
-    public static final String VERSION = "0.1.0-beta1";
+    public static final String VERSION = "0.1.0-alpha.1";
     public static final String MODNAME = "Food Mod";
     
     // Initialize item fields
